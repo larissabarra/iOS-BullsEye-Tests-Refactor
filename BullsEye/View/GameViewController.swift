aa41f9a809e79081f6abfa86ea9cf891216a9ec9
Larissa <lbarra@thoughtworks.com>
2018-04-11 09:47:29 -0300

    [logic] lbarra: adds new view class to game screen

---
+++ BullsEye/View/GameViewController.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+class GameViewController: UIViewController {
+  @IBOutlet weak var slider: UISlider!
+  @IBOutlet weak var targetLabel: UILabel!
+  @IBOutlet weak var scoreLabel: UILabel!
+  @IBOutlet weak var roundLabel: UILabel!
+  @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+  
+  @IBAction func hitMeTapped() {
+    
+  }
+  
+  @IBAction func sliderMoved(_ slider: UISlider) {
+    
+  }
+  
+  @IBAction func startNewGame() {
+    
+  }
+}
